7ff1cf97e818be6d92c98af220a90817cab7c24f
MK <simon365@naver.com>
2023-03-04 14:52:43 +0900

    Modify enemy movement
    instead of rigidbody, use vector

--- Assets/2. Scripts/Enemy.cs
+++ Assets/2. Scripts/Enemy.cs
@@ -15,41 +15,29 @@ public class Enemy : MonoBehaviour
     Rigidbody2D rigid;
     bool isEnemyLive;
 
+    Vector2 playerPos = new Vector2(0.0f, 1.3f);
+    
 
-    void Awake()
+    private void Start()
     {
-        player = GameManger.instance.player.GetComponent<Rigidbody2D>();
-        rigid = GetComponent<Rigidbody2D>();
-        LookPlayer();
+        float targetAngle = Vector2.Angle(transform.up, playerPos - (Vector2)transform.position);
+        targetAngle = transform.position.x >= 0 ? targetAngle : -targetAngle;
+        transform.Rotate(new Vector3(0, 0, targetAngle));
+    }
 
+    private void OnEnable()
+    {
+        float targetAngle = Vector2.Angle(transform.up, playerPos - (Vector2)transform.position);
+        targetAngle = transform.position.x >= 0 ? targetAngle : -targetAngle;
+        transform.Rotate(new Vector3(0, 0, targetAngle));
     }
 
     void FixedUpdate()
     {
-        Move();
+        transform.position += transform.up * Time.deltaTime * enemySpeed;
     }
 
-    private void Move()
-    {
-        Vector2 dirVector = player.position - rigid.position;
-
-        //Vector2 dirVector = new Vector2(player.transform.position.x - rigid.position.x, player.transform.position.y - rigid.position.y);
-        Vector2 nextVector = dirVector.normalized * enemySpeed * Time.fixedDeltaTime;
-        rigid.MovePosition(rigid.position + nextVector);
-        rigid.velocity = Vector2.zero;
-    }
-    
-    public void LookPlayer()
-    {
-        //Debug.Log("target Position : " + player.position);
-        //Debug.Log("this Position : " + rigid.position);
-        //Debug.Log("this transform position " + this.gameObject.name);
-        Vector2 dirVector = player.position - rigid.position;
-        float lookAngle = Mathf.Atan2(dirVector.y, dirVector.x) * Mathf.Rad2Deg;
-        Quaternion angleAxis = Quaternion.AngleAxis(lookAngle - 90f, Vector3.forward);
-        transform.rotation = angleAxis;
-        //Debug.Log("This Rotation  : " + transform.rotation);
-    }
+ 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if(other.gameObject.tag == "Bullet")
