a467e64666e76ab4c4931380e5fc6b5411adda6f
MK <simon365@naver.com>
2023-03-09 22:18:58 +0900

    Lobby UI Fragment Change Anim
    +Fragment Change Anim
    +Parts UI

--- Assets/2. Scripts/LobbyUiManager.cs
+++ Assets/2. Scripts/LobbyUiManager.cs
@@ -5,20 +5,41 @@ using UnityEngine.SceneManagement;
 
 public class LobbyUiManager : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    #region UI Components
+    [SerializeField] private GameObject mainUI;
+    [SerializeField] private GameObject currentFragment;
+    [SerializeField] private GameObject nextFragment;
+    [SerializeField] private Animation fragmentChangeAnim;
+    #endregion
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    private GameObject target;
 
     public void ChangeScene()
     {
         SceneManager.LoadScene("InGameScene");  
+    }
+
+    public void OnClickFragmentChange(GameObject targetFragment)
+    {
+        if (currentFragment.transform.GetChild(0) == targetFragment.transform)
+            return;
+        target = targetFragment;
+        targetFragment.transform.parent = nextFragment.transform;
+        targetFragment.GetComponent<RectTransform>().anchoredPosition = new Vector2(0, 0);
+
+        fragmentChangeAnim.Play("FragmentChangeAnim");
+        Invoke("OnEndChangeAnimation", 1f);
+    }
+
+    public void OnEndChangeAnimation()
+    {
+        target.transform.parent = mainUI.transform;
+        currentFragment.transform.GetChild(0).transform.parent = mainUI.transform;
+
+        currentFragment.GetComponent<RectTransform>().anchoredPosition = new Vector2(0, 0);
+        nextFragment.GetComponent<RectTransform>().anchoredPosition = new Vector2(1440, 0);
+
+        target.transform.parent = currentFragment.transform;
+        target.GetComponent<RectTransform>().anchoredPosition = new Vector2(0, 0);
     }
 }
