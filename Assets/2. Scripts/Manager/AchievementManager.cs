1dd55484a9ec38f566b8b2168d682669f75ea976
leejy811 <lee38956162@gmail.com>
2023-04-14 20:18:04 +0900

    Achievement Manager 주석 작성

--- Assets/2. Scripts/Manager/AchievementManager.cs
+++ Assets/2. Scripts/Manager/AchievementManager.cs
@@ -2,6 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// IAchieve 클래스는 업적 정보의 인터페이스를 제공하는 클래스이다.
+/// </summary>
 [System.Serializable]
 public class IAchieve
 {
@@ -20,13 +23,21 @@ public class IAchieve
     }
 }
 
+/// <summary>
+/// AchievementManager는 업적들의 정보를 저장하고 관리하는 변수이다. achieves에는 업적의 정보가 저장되어 있고 
+/// Key 값은 업적의 이름으로 되어있다. achieves의 정보를 접근할땐 예를들어 achieves["Starting from the basic"].achieveName이렇게 호출하면 된다.
+/// 업적의 성공여부는 isSuccess함수의 반환 값을 사용하면 된다.
+/// </summary>
 public class AchievementManager : MonoBehaviour
 {
+    //Singleton Pattern
     public static AchievementManager instance;
 
+    //업적의 정보가 모두 들어있는 Dictionary
     public Dictionary<string, IAchieve> achieves = new Dictionary<string,IAchieve>();
     private string[] achieveNames;
 
+    //Execl안의 정보를 가져온 DataBase
     public DataBase achieveDB;
 
     private void Awake()
@@ -50,6 +61,7 @@ public class AchievementManager : MonoBehaviour
         LoadAchieve();
     }
 
+    //LocalDataBase에서 업적 정보를 가져오는 함수
     public void LoadAchieve()
     {
         LocalDatabaseManager.instance.LoadAchieveData();
@@ -61,6 +73,7 @@ public class AchievementManager : MonoBehaviour
         }
     }
 
+    //LocalDataBase에 업적 정보를 저장하는 함수
     public void SaveAchieve()
     {
         for (int i = 0; i < achieves.Count; i++)
@@ -72,6 +85,7 @@ public class AchievementManager : MonoBehaviour
         LocalDatabaseManager.instance.SaveAchieveData();
     }
 
+    //achieves의 Key값을 초기화 해주는 함수
     public void LoadAchieveStringIndex()
     {
         achieveNames = new string[achieveDB.Achieves.Count];
@@ -82,6 +96,7 @@ public class AchievementManager : MonoBehaviour
         }
     }
 
+    //Execl에서 업적 정보를 가져올 때 변수를 할당해주는 함수다.
     public void LoadAchieveInfo()
     {
         string[] tempCondition;
@@ -105,6 +120,7 @@ public class AchievementManager : MonoBehaviour
         }
     }
 
+    //파츠 업적 정보를 저장하는 함수다.
     public void SavePartsAchieve()
     {
         if (LocalDatabaseManager.instance.PartsMissile[2] == 1)
